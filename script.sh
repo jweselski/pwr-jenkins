0da3bc32ef6ed9fcb1cca088cc5d95f780afcede
Jarek Weselski <weselski.jarek@gmail.com>
2018-04-04 22:42:00 +0200

    remove unused stuff

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e 'UPDATE core_config_data SET value = '" $FACT_FINDER_ACTIVATION "'  WHERE path = "factfinder/general/is_enabled"' $MYSQL_DATABASE
